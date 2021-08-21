cc94bde3109e7099cccf421685fe668ec928e44b
isaaq1235 <72512225+isaaq1235@users.noreply.github.com>
2021-08-21 18:07:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
-for row in rows+5:
-    st.write(f" Rows = {row.Code}")
+for row in rows:
+    st.write(f"{row}")
