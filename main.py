f92fb2e8c18f8eb5673fd3fff0ad391e74fec44f
isaaq1235 <72512225+isaaq1235@users.noreply.github.com>
2021-08-21 16:06:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import base64
+from gsheetsdb import connect
 
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
 # st.image("trginslogo.png")
 
-st.title("Rate Analysis")
+# st.title("Rate Analysis")
 
 # df = pd.read_excel('Concrete Itms - Foundation, Column, Slab & Beam, Walls Works (1).xls')
 
