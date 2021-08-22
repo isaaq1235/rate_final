0a17456dba9132f6bcb3a9d831dd762f29c649a9
isaaq1235 <72512225+isaaq1235@users.noreply.github.com>
2021-08-22 13:03:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import streamlit as st
-import numpy as np
-import psycopg2
+import mysql.connector
 
-st.title('Rate Analysis')
-
+# Initialize connection.
+# Uses st.cache to only run once.
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return psycopg2.connect(host = "localhost",port = 5432,dbname = "rate_analysis",user = "postgres",password = "isaaq2021")
+    return mysql.connector.connect(**st.secrets["mysql"])
 
-con = init_connection()
+conn = init_connection()
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=60)
+@st.cache(ttl=600)
 def run_query(query):
-    with con.cursor() as cur:
+    with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from rate_dat;")
+rows = run_query("SELECT * from rate_data;")
 
 # Print results.
 for row in rows:
