6242897ba81dba4ee647c9a311afb72fe2af4d45
isaaq1235 <72512225+isaaq1235@users.noreply.github.com>
2021-08-22 13:33:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import mysql.connector
 # Uses st.cache to only run once.
 # @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 
-conn = mysql.connector.connect(host = 127.0.0.1,port = 3306,user = 'root',password = 'isaaq2021')
+conn = mysql.connector.connect(host = '127.0.0.1',port = 3306,user = 'root',password = 'isaaq2021')
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
